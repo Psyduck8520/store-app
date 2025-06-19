c751ce058be1551ffb563ee848bc51f3b1889ba7
Edison Quimbiamba <exprogresive@gmail.com>
2025-06-19 13:28:47 -0500

    Class 5 , Create and reuse of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Product} from "./models/product.model";
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = '';
 
-  onLoaded( img: string){
-     console.log('Log  padre', img);
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Mis libors ',
+      image: 'assets/images/books.jpg',
+      price: 100,
+    },
+    {
+      id: '2',
+      name: 'El mejor juguete',
+      image: 'assets/images/toy.jpg',
+      price: 100
+    },
+    {
+      id: '3',
+      name: 'Biciclita casi nueva',
+      image: 'assets/images/bike.jpg',
+      price: 200
+    },
+    {
+      id: '4',
+      name: 'Collección  de albummes',
+      image: 'assets/images/album.jpg',
+      price: 100,
+    },
+    {
+      id: '5',
+      name: 'Mis libors ',
+      image: 'assets/images/books.jpg',
+      price: 100,
+    },
+    {
+      id: '6',
+      name: 'My House ',
+      image: 'assets/images/house.jpg',
+      price: 100,
+    },
+    {
+      id: '7',
+      name: 'My House ',
+      image: 'assets/images/glasses.jpg',
+      price: 100,
+    }
+  ]
+
+  imgParent = '';
+  onLoaded(img: string) {
+    console.log('Log  padre', img);
   }
 }
