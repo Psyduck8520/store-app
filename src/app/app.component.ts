413c559a77ef704667a997c57b543ee9f85fd820
Edison Quimbiamba <exprogresive@gmail.com>
2025-06-23 22:40:04 -0500

    Class , add product list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,52 +11,6 @@ export class AppComponent {
   imgParent = '';
   showImg   = true;
 
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'Mis libors ',
-      image: 'assets/images/books.jpg',
-      price: 100,
-    },
-    {
-      id: '2',
-      name: 'El mejor juguete',
-      image: 'assets/images/toy.jpg',
-      price: 100
-    },
-    {
-      id: '3',
-      name: 'Biciclita casi nueva',
-      image: 'assets/images/bike.jpg',
-      price: 200
-    },
-    {
-      id: '4',
-      name: 'Collección  de albummes',
-      image: 'assets/images/album.jpg',
-      price: 100,
-    },
-    {
-      id: '5',
-      name: 'Mis libors ',
-      image: 'assets/images/books.jpg',
-      price: 100,
-    },
-    {
-      id: '6',
-      name: 'My House ',
-      image: 'assets/images/house.jpg',
-      price: 100,
-    },
-    {
-      id: '7',
-      name: 'My House ',
-      image: 'assets/images/glasses.jpg',
-      price: 100,
-    }
-  ]
-
   onLoaded(img: string) {
     console.log('Log  padre', img);
   }
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {Product} from "../../models/product.model";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Mis libors ',
+      image: 'assets/images/books.jpg',
+      price: 100,
+    },
+    {
+      id: '2',
+      name: 'El mejor juguete',
+      image: 'assets/images/toy.jpg',
+      price: 100
+    },
+    {
+      id: '3',
+      name: 'Biciclita casi nueva',
+      image: 'assets/images/bike.jpg',
+      price: 200
+    },
+    {
+      id: '4',
+      name: 'Collección  de albummes',
+      image: 'assets/images/album.jpg',
+      price: 100,
+    },
+    {
+      id: '5',
+      name: 'Mis libors ',
+      image: 'assets/images/books.jpg',
+      price: 100,
+    },
+    {
+      id: '6',
+      name: 'My House ',
+      image: 'assets/images/house.jpg',
+      price: 100,
+    },
+    {
+      id: '7',
+      name: 'My House ',
+      image: 'assets/images/glasses.jpg',
+      price: 100,
+    }
+  ]
+}
