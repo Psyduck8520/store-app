e40ce46741022cf97f40f531e2efeccf4c36639e
Edison Quimbiamba <exprogresive@gmail.com>
2025-07-06 17:54:49 -0500

    Class 13 -14, httpClient Services

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Product} from "../../models/product.model";
 import {StoreService} from "../../services/store.service";
+import  {ProductsService} from "../../services/products.service";
 
 @Component({
   selector: 'app-products',
@@ -10,56 +11,20 @@ import {StoreService} from "../../services/store.service";
 export class ProductsComponent {
   myShoppingCart: Product[] = [];
   total = 0;
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'Mis libors ',
-      image: 'assets/images/books.jpg',
-      price: 100,
-    },
-    {
-      id: '2',
-      name: 'El mejor juguete',
-      image: 'assets/images/toy.jpg',
-      price: 100
-    },
-    {
-      id: '3',
-      name: 'Biciclita casi nueva',
-      image: 'assets/images/bike.jpg',
-      price: 200
-    },
-    {
-      id: '4',
-      name: 'Collección  de albummes',
-      image: 'assets/images/album.jpg',
-      price: 100,
-    },
-    {
-      id: '5',
-      name: 'Mis libors ',
-      image: 'assets/images/books.jpg',
-      price: 100,
-    },
-    {
-      id: '6',
-      name: 'My House ',
-      image: 'assets/images/house.jpg',
-      price: 100,
-    },
-    {
-      id: '7',
-      name: 'My House ',
-      image: 'assets/images/glasses.jpg',
-      price: 100,
-    }
-  ]
+  products: Product[] = []
 
   constructor(
     private storeService: StoreService,
+    private ProductsService: ProductsService
   ) {
       this.myShoppingCart = this.storeService.getShoppingCart();
   }
+  ngOnInit() {
+     this.ProductsService.getAllProducts().subscribe(products => {
+       this.products =  products;
+     });
+  }
+
 
   onAddToShoppingCart(product: Product) {
     console.log(product);
