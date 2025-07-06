67c22cee7255852ee4f6ebe05569fbfdfa57b88c
Edison Quimbiamba <exprogresive@gmail.com>
2025-07-06 11:17:15 -0500

    Class 10 , lmplement side menu a rese style

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent  implements OnInit {
+   activeMenu = false;
+  constructor() {
+  }
+  ngOnInit() { }
+  toggleMenu() {
+     this.activeMenu = !this.activeMenu;
+  }
 
 }
